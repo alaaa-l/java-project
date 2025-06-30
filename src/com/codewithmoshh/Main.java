7d21e61c58616133297b984c805cf0f3427d6a32
alaaa-l <lezzeikalaa@gmail.com>
2025-06-30 07:01:47 +0300

    FizzBuzz Code

--- src/com/codewithmoshh/Main.java
+++ src/com/codewithmoshh/Main.java
@@ -1,28 +1,20 @@
 package com.codewithmoshh;
 
-import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-       final byte MONTHS_IN_YEAR=12;
-       final byte PERCENT=100;
-       Scanner scanner= new Scanner(System.in);
-       System.out.print("Principal: ");
-       int principal= scanner.nextInt();
+       Scanner scanner=new Scanner(System.in);
+       System.out.print("Number: ");
+       int number=scanner.nextInt();
+       if(number%5==0)
+           System.out.print("Fizz");
+       if(number%3==0)
+           System.out.print("Buzz");
+       if(number%5!=0 && number%3!=0)
+           System.out.println(number);
 
-       System.out.print("Annual Interest Rate: ");
-       float annualInterestRate= scanner.nextFloat();
 
-       System.out.print("Period (Years): ");
-       byte years= scanner.nextByte();
-
-       float r= (annualInterestRate/PERCENT)/MONTHS_IN_YEAR; //monthly interest rate
-       int n=years*MONTHS_IN_YEAR; //number of payments
-       double m=(principal*r*Math.pow((1+r),n))/(Math.pow((1+r),n)-1);
-       NumberFormat currency=NumberFormat.getCurrencyInstance();
-       String mortgage=currency.format(m);
-       System.out.println("Mortgage: "+mortgage);
 
     }
 }

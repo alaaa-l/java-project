e02d88b92feb232da56eaf1e6c2244c9369e10fa
alaaa-l <lezzeikalaa@gmail.com>
2025-06-30 04:46:18 +0300

    first mortgage calculator code

--- src/com/codewithmoshh/Main.java
+++ src/com/codewithmoshh/Main.java
@@ -1,7 +1,28 @@
 package com.codewithmoshh;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World");
+       final byte MONTHS_IN_YEAR=12;
+       final byte PERCENT=100;
+       Scanner scanner= new Scanner(System.in);
+       System.out.print("Principal: ");
+       int principal= scanner.nextInt();
+
+       System.out.print("Annual Interest Rate: ");
+       float annualInterestRate= scanner.nextFloat();
+
+       System.out.print("Period (Years): ");
+       byte years= scanner.nextByte();
+
+       float r= (annualInterestRate/PERCENT)/MONTHS_IN_YEAR; //monthly interest rate
+       int n=years*MONTHS_IN_YEAR; //number of payments
+       double m=(principal*r*Math.pow((1+r),n))/(Math.pow((1+r),n)-1);
+       NumberFormat currency=NumberFormat.getCurrencyInstance();
+       String mortgage=currency.format(m);
+       System.out.println("Mortgage: "+mortgage);
+
     }
 }
